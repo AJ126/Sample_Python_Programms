6438c92d3991c5445b33a894648cec9e829e763e
Akash Dattatray Jagtap <64182118+AJ126@users.noreply.github.com>
2021-05-26 23:45:17 +0530

    Delete userdata directory

--- userdata/user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from user.models import *
-# Register your models here.
-class userinfoAdmin(admin.ModelAdmin):
-    list_display=['name','username','password','street','pincode','country','state','phoneno']
-admin.site.register(userinfo,userinfoAdmin)
